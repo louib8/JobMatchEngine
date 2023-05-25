c42b4aad87231fdb8929195127b45a0f950e9251
louib8 <louis.i.brown.8@gmail.com>
2023-05-25 13:32:10 +1000

    Filled out JobSeekerModel class

--- Model/JobSeekerModel.cs
+++ Model/JobSeekerModel.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace JobMatchEngine.Model
 {
-    internal class JobSeekerModel
+    public class JobSeekerModel
     {
+        private string _id;
+        private string _name;
+        private List<string> _skills = new List<string>();
+
+        public string ID;
+        public string Name;
+        public List<string> Skills = new List<string>();
+
+
+        public JobSeekerModel(string id, string name, List<string> skills)
+        {
+            _id = id;
+            _name = name;
+            _skills = skills;
+        }
     }
 }
