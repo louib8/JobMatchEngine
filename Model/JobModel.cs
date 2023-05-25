546a7b159a1da44a699f9abfb69030562c281128
louib8 <louis.i.brown.8@gmail.com>
2023-05-25 13:37:59 +1000

    Populated jobmodel class

--- Model/JobModel.cs
+++ Model/JobModel.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace JobMatchEngine.Model
 {
-    internal class JobModel
+    public class JobModel
     {
+        private string _id;
+        private string _name;
+        private List<string> _skills = new List<string>();
+
+        public string ID { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public List<string> Skills { get => _skills; set => _skills = value; }
+
+        public JobModel(string id, string name, List<string> skills)
+        {
+            _id = id;
+            _name = name;
+            _skills = skills;
+        }
     }
 }
